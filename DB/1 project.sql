e6178be21ad382ea38872c8b7b946495e39a92b2
HelenBoitsova <helen.boitsova@gmail.com>
2016-03-25 22:18:03 -0400

    Helens changes

--- DB/1 project.sql
+++ DB/1 project.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2016 at 03:17 AM
+-- Generation Time: Mar 26, 2016 at 03:11 AM
 -- Server version: 10.1.9-MariaDB
 -- PHP Version: 5.6.15
 
