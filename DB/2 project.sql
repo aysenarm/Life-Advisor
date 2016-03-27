931122cb784e94a230f441eb30d20fa20a8dd930
AntonioFreire <amanuelfreire@gmail.com>
2016-03-26 20:12:28 -0400

    db test

--- DB/2 project.sql
+++ DB/2 project.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Mar 27, 2016 at 01:10 AM
+-- Host: 127.0.0.1
+-- Generation Time: Mar 26, 2016 at 03:17 AM
 -- Server version: 10.1.9-MariaDB
--- PHP Version: 7.0.1
+-- PHP Version: 5.6.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
