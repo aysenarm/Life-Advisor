a51911289018eee07cb9be003264ea35fed0fc60
Alexander <alex.volynchikov@mail.ru>
2016-03-30 10:58:03 -0400

    Fake index page for my features

--- Alex/index_temp.php
+++ Alex/index_temp.php
@@ -2,14 +2,19 @@
 
 <title>Index Temporary</title>
 
-
     <h2>Donations</h2>
+        <h3>User page</h3>
+            <p><a href="donation_add_form.php">Add donation</a></p>
 
-    <h3>User page</h3>
-    <p><a href="donation_add_form.php">Add donation</a></p>
+        <h3>Admin page</h3>
+            <p><a href="donation_admin.php">View list of donations - Change donations - Update donations</a></p>
 
-    <h3>Admin page</h3>
-    <p><a href="donation_admin.php">View list of donations / Change donations / Update donations</a></p>
 
+    <h2>Contact Us</h2>
+        <h3>User personal page</h3>
+            <p><a href="">The personal user page with user questions - Add question - Delete user questions</a></p>
+
+         <h3>Admin page</h3>
+            <p><a href="donation_admin.php">Answer/Delete Questions</a></p>
 
 <?php require_once '../content_bottom.php'; ?>
