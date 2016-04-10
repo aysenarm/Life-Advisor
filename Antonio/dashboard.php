0c6019f89bb36224d854fb92d33af32824e03897
AntonioFreire <amanuelfreire@gmail.com>
2016-04-10 18:47:22 -0400

    Register/Login

--- Antonio/dashboard.php
+++ Antonio/dashboard.php
@@ -3,14 +3,13 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Welcome Home</title>
+    <title>Dashboard</title>
     <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
 <div class="form">
-    <p>Welcome <?php echo $_SESSION['username']; ?>!</p>
-    <p>This is secure area.</p>
-    <p><a href="dashboard.php">Dashboard</a></p>
+    <p>This is another secured page.</p>
+    <p><a href="index.php">Home</a></p>
     <a href="logout.php">Logout</a>
 </div>
 </body>
