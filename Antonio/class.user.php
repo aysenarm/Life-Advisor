542fa490450eb39fbeb72bfc0ba904284fa03a11
AntonioFreire <amanuelfreire@gmail.com>
2016-04-11 15:14:44 -0400

    nithia fix

--- Antonio/class.user.php
+++ Antonio/class.user.php
@@ -14,7 +14,7 @@ class USER
         {
             $new_password = password_hash($upass, PASSWORD_DEFAULT);
 
-            $stmt = $this->db->prepare("INSERT INTO users(user_name,user_email,user_pass)
+            $stmt = $this->db->prepare("INSERT INTO user(Username,E-mail,Password)
                                                        VALUES(:uname, :umail, :upass)");
 
             $stmt->bindparam(":uname", $uname);
@@ -34,14 +34,14 @@ class USER
     {
         try
         {
-            $stmt = $this->db->prepare("SELECT * FROM users WHERE user_name=:uname OR user_email=:umail LIMIT 1");
+            $stmt = $this->db->prepare("SELECT * FROM user WHERE Username=:uname OR E-mail=:umail LIMIT 1");
             $stmt->execute(array(':uname'=>$uname, ':umail'=>$umail));
             $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
             if($stmt->rowCount() > 0)
             {
-                if(password_verify($upass, $userRow['user_pass']))
+                if(password_verify($upass, $userRow['Password']))
                 {
-                    $_SESSION['user_session'] = $userRow['user_id'];
+                    $_SESSION['user_session'] = $userRow['ID user'];
                     return true;
                 }
                 else
--- Antonio/profile.php
+++ Antonio/profile.php
@@ -8,8 +8,8 @@
 	
 	$user_id = $_SESSION['user_session'];
 	
-	$stmt = $auth_user->runQuery("SELECT * FROM users WHERE user_id=:user_id");
-	$stmt->execute(array(":user_id"=>$user_id));
+	$stmt = $auth_user->runQuery("SELECT * FROM user WHERE Rights=:Rights");
+	$stmt->execute(array(":Rights"=>$user_id));
 	
 	$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
 	
