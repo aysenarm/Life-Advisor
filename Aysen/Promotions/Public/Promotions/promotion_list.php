174b1f4f2577ed1907ab1db5c08d75746f3ae632
AntonioFreire <amanuelfreire@gmail.com>
2016-04-28 11:44:39 -0400

    more promotions

--- Aysen/Promotions/Public/Promotions/promotion_list.php
+++ Aysen/Promotions/Public/Promotions/promotion_list.php
@@ -1,40 +1,15 @@
 <div id="main">
-    <div>
-<h1>Promotions</h1>
-        <div class="photos">
-
-
-            <?php foreach ($promotions as $promotion) : ?>
+    <h1>Promotions</h1>
+    <div class="row">
+        <?php foreach ($promotions as $promotion) : ?>
             <div class="col-md-6">
                 <div class="well">
-                    <img class="img-responsive" src="../../Admin/Images/photo_gallery/<?php echo $promotion->getImage();?>" alt="Cover of <?php echo $promotion->getTitle();?>" width="304" height="236" >
+                    <img class="img-responsive text-center" src="../../Admin/Images/photo_gallery/<?php echo $promotion->getImage();?>" alt="Cover of <?php echo $promotion->getTitle();?>" >
                     <h3><?php echo $promotion->getTitle();?></h3>
                     <h5>Available : <?php echo $promotion->getDateValid();?></h5>
                     <h4>Promotion Code :<?php echo  $promotion->getKey();?></h4>
                 </div>
             </div>
-            <?php endforeach; ?>
-
-        </div>
+        <?php endforeach; ?>
     </div>
-
 </div>
-<style>
-
-    ul {
-        list-style-type: none;
-    }
-
-    li img {
-        float: left;
-        margin: 5px;
-        border: 5px solid #333;
-        -moz-transition: all 0.2s ease-in-out;
-        -webkit-transition: all 0.2s ease-in-out;
-        transition: all 0.2s ease-in-out;
-    }
-
-    li img:hover {
-        border: 5px solid #F15123;
-    }
-</style>
